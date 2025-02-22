9134a0118c084a575fe2b90959128c708a16f1a0
Mayank Pandey <immayank7781@gmail.com>
2025-02-23 03:05:39 +0530

    Room

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,74 +1,104 @@
-import { useEffect, useRef, useState } from "react";
+import { SetStateAction, useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Room = () => {
 
-    const [messages, setMessages] = useState(["Welcome to the chat!"]);
-    const inputRef = useRef(null);
-    const userMessageRef = useRef("");
-    const wsRef = useRef(null);
+    const [socket, setSocket] = useState<WebSocket | null>(null);
+    const [messages, setMessages] = useState<string[]>([]);
+    const [userMessage, setUserMessage] = useState<string>("");
+    const [sender, setSender] = useState<string>("");
+    const [leaveRoom , setLeaveRoom] = useState(false);
+    const navigate = useNavigate();
+
+
+    // User Data
+    // @ts-ignore
+    const user = JSON.parse(localStorage.getItem("user") || "{}");
+    if(!user.name || !user.roomId){
+        navigate("/");
+    }
 
     useEffect(()=>{
-        const ws = new WebSocket("http://localhost:8000");
-        ws.onmessage = (event)=>{
-            console.log(event);
-            setMessages(m => [...m, event.data])
-        }
-        // @ts-ignore
-        wsRef.current = ws;
+        const ws = new WebSocket("http://localhost:8000"); // Connect on backend server
+        setSocket(ws);
 
-        ws.onopen=()=>{
+        ws.onopen = () => {
+            console.log("Connected to websocket server");
             ws.send(JSON.stringify({
                 type : "join",
-                payload : {
-                    roomId : "123"
-                }
+                // @ts-ignore
+                payload : localStorage.getItem("user")
             }))
         }
-    }, [])
+
+        ws.onmessage = (event)=>{
+            const imp = JSON.parse(event.data);
+            setSender(imp.payload.sender);
+            const message = imp.payload.message;
+            setMessages((prev)=>[...prev, message]);
+        }
+
+        return () => ws.close();
+    }, [leaveRoom])
+
+
 
     function sendMessageHandler(){
+
+        if(!userMessage.trim() || !socket){
+            return;
+        }
         
-        // @ts-ignore
-        let message = inputRef.current.value;
-        // @ts-ignore
-        userMessageRef.current = inputRef.current.value;
+        socket.send(
+            JSON.stringify({
+                type : "chat",
+                payload : {
+                    message : userMessage,
+                    user : user
+                }
+            })
+        )
+        setUserMessage("");
+    }
 
-        // @ts-ignore
-        wsRef.current.send(JSON.stringify({
-            type : "chat",
-            payload : {
-                message : message
-            }
-        }))
-
-        // @ts-ignore
-        inputRef.current.value = "";
+    function leaveRoomHandler(){
+        localStorage.removeItem("user");
+        setLeaveRoom(true);
     }
 
   return (
     <div className="w-full h-[100%] flex overflow-hidden">
         {/* Left */}
-        <div className="border-r-[1px] border-[#8D8D92] w-[50%] flex items-center justify-center">
-            <div className="text-5xl text-white font-mono py-5">
-                Chat Room
+        <div className="w-[50%] flex flex-col items-center justify-center font-mont bg-[#212227]">
+            <div className="text-3xl text-white py-5">
+                Chat Room : {user.roomId}
+            </div>
+
+            <div onClick={leaveRoomHandler} className="text-white bg-gradient-to-r from-[#000] to-[#F18805] px-3 py-2 cursor-pointer">
+                Leave Chat Room
             </div>
         </div>
 
 
         {/* Right */}
-        <div className="border w-[50%] flex justify-center items-center">
-            <div className="w-[90%] h-[90%] flex flex-col gap-2 justify-between border-[1px] rounded-2xl text-white py-5">
+        <div className="w-[50%] flex flex-col justify-center items-center py-4 bg-[#0A0A0A]">
+            <div className="h-[10%] flex items-center justify-center text-white font-mont text-2xl">
+                <div>
+                    Chat Room : {user.name}
+                </div>
+            </div>
+            <div className="w-[90%] h-[90%] flex flex-col gap-2 justify-between border-[1px] text-white py-5">
                 {/* Chats */}
-                <div className="px-5 font-mono overflow-scroll">
+                <div className="px-5 font-mono overflow-scroll flex flex-col gap-2">
                     {
-                        messages.map((message)=> <div>{message === userMessageRef.current ? <div className="bg-amber-400">{message}</div> : <div className="bg-blue-500">{message}</div>}</div>)
+                        messages.map((message, index)=> <div key={index} className="w-full">{user.name === sender ? <div className="w-full flex justify-end"><div className="bg-amber-400 w-fit text-right">{message}</div></div> : <div><div className="bg-blue-500 inline-block">{message}</div></div>}</div>)
                     }
                 </div>
 
                     {/* Firm */}
                 <div className="w-[100%] flex gap-2 px-5 font-mono">
-                    <input type="text" placeholder="Enter your message" className="border border-[#E8E9ED] text-[#E8E9ED] rounded-2xl outline-0 flex-grow px-3 py-2" ref={inputRef}/>
-                    <button className="cursor-pointer bg-[#2D3142] hover:bg-[#E8E9ED] transition-all duration-300 hover:text-[#000] px-5 py-2 rounded-xl" onClick={sendMessageHandler}>Send</button>
+                    <input type="text" placeholder="Enter your message" className="border border-[#E8E9ED] text-[#E8E9ED] rounded-xl outline-0 flex-grow px-3 py-2" onChange={(e)=> setUserMessage(e.target.value)} value={userMessage}/>
+                    <button className="cursor-pointer bg-[#2D3142] hover:bg-[#E8E9ED] transition-all duration-300 hover:text-[#000] px-5 py-2" onClick={sendMessageHandler}>Send</button>
                 </div>
             </div>
         </div>
